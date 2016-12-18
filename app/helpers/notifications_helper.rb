6f887ceed779cd383bcbd313356833b6d1e120c0
yhayashi30 <take_it_easy881128@ybb.ne.jp>
2016-12-18 06:01:08 +0000

    通知から遷移を修正

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,4 +2,12 @@ module NotificationsHelper
     def posted_time(time)
         time > Date.today ? "#{time_ago_in_words(time)}" : time.strftime('%m月%d日')
     end
+    
+    def read_change(read)
+        if read
+          '既読'  
+        else
+          '未読'
+        end
+    end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,12 +1,4 @@
 class Notification < ActiveRecord::Base
   belongs_to :user
   belongs_to :comment
-  
-  def read_change(read)
-    if read
-      '既読'  
-    elsif
-      '未読'
-    end
-  end
 end
